ca5105da66e699da2e351671548cfc131ba4e775
Tomaz Martins <tomaz.unb@outlook.com>
2013-10-06 01:41:58 -0300

    Revert "Implementação de funções de travessia"
    This reverts commit 4b9802bd38e4a173cbd466daa9e8f3107d46b87a.

--- moduloTomaz.hs
+++ moduloTomaz.hs
@@ -1,21 +1,6 @@
 module ArvoreBinaria
 ( ArvoreBinaria
-, preOrdem
-, emOrdem
-, posOrdem
 ) where
 
 data ArvoreBinaria elemento = NULL
 		| No elemento ( ArvoreBinaria elemento )( ArvoreBinaria elemento )
-
-preOrdem :: ArvoreBinaria elemento -> [ elemento ]
-preOrdem NULL = [ ]
-preOrdem ( No elem esq dir ) = [ elem ] ++ ( preOrdem esq ) ++ (preOrdem dir )
-
-emOrdem :: ArvoreBinaria elemento -> [ elemento ]
-emOrdem NULL = [ ]
-emOrdem ( No elem esq dir ) = ( preOrdem esq ) ++ [ elem ] ++ (preOrdem dir )
-
-posOrdem :: ArvoreBinaria elemento -> [ elemento ]
-posOrdem NULL = [ ]
-posOrdem ( No elem esq dir ) = ( preOrdem esq ) ++ (preOrdem dir ) ++ [ elem ]
