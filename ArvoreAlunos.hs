31322dcd536c83f64079c56bb1e15f769aa36b19
ricardogtx <ricardocdzg@hotmail.com>
2013-10-06 19:49:00 -0300

    funcoes

--- ArvoreAlunos.hs
+++ ArvoreAlunos.hs
@@ -1,7 +1,27 @@
-module ArvoreAlunos where
+module ArvoreAlunos 
+(	insereAluno
+,	excluiAluno
+)where
 
 import Aluno
 
+
+
 data ArvoreAlunos = NULL
-		| No Aluno ( ArvoreAlunos )( ArvoreAlunos )
+		| No Aluno (ArvoreAlunos)(ArvoreAlunos)
 	deriving( Show, Eq, Read )
+
+
+insereAluno::Aluno->ArvoreAlunos->ArvoreAlunos 
+insereAluno x NULL = (No x NULL NULL)
+insereAluno x (No y esq dir) = No y esq (insereAluno x dir)
+
+excluiAluno::Aluno->ArvoreAlunos->ArvoreAlunos
+excluiAluno x NULL = NULL
+excluiAluno x (No y esq dir)
+--	| x < y = No y (removeGen x esq) dir
+--	| x > y = No y esq (removeGen x dir)
+	| x == y = NULL
+	| otherwise = NULL
+	
+	
