e70ee0ad6904e00a75f582cbcfe8bf143c53214c
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-07 03:24:18 -0300

    Função para esvaziar árvore.

--- VerificaVazia.hs
+++ VerificaVazia.hs
@@ -1,12 +1,17 @@
 module VerificaVazia 
 ( verificaVazia
+, esvaziaArvore
 )where
 
 import Aluno
 import ArvoreAlunos
 
-verificaVazia::ArvoreAlunos->Bool
+verificaVazia :: ArvoreAlunos->Bool
 verificaVazia NULL = True
 verificaVazia (No a esq dir)
 	|No a esq dir == NULL = True
 	|No a esq dir /= NULL = False
+
+esvaziaArvore :: ArvoreAlunos -> ArvoreAlunos
+esvaziaArvore NULL = NULL
+esvaziaArvore (No a esq dir) = NULL
