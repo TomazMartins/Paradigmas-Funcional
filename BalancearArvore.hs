b1391f6d725c9a0a989886ba69e5e8de7459271e
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-07 02:51:54 -0300

    Função balancear completa.

--- BalancearArvore.hs
+++ BalancearArvore.hs
@@ -1,18 +1,40 @@
 module BalancearArvore
-( estaBalanceada
-, alturaArvore
+( alturaArvore
+, estaBalanceada
+, rotacionarDir
+, rotacionarEsq
+, diferencaAltura
 , balancear
 ) where
 
 import ArvoreAlunos
+import Aluno
 
-alturaArvore :: ArvoreAlunos a -> Int
+alturaArvore :: ArvoreAlunos -> Int
 alturaArvore NULL = 0
-alturaArvore (No a arvEsq arvDir) = 1 + max (alturaArvora arvEsq) (alturaArvore arvDir)
+alturaArvore (No a arvEsq arvDir) = 1 + max (alturaArvore arvEsq) (alturaArvore arvDir)
 
-estaBalanceada :: ArvoreAlunos a -> Bool
-estaBalanceada NULL = true
-estaBalanceada (No a arvEsq arvDir) = (-1 <= (alturaArvore arvDir) - (alturaArvore arvEsq) <= 1) && (estaBalanceada arvDir) && (estaBalanceada arvEsq)
+estaBalanceada :: ArvoreAlunos -> Bool
+estaBalanceada NULL = True
+estaBalanceada (No a arvEsq arvDir) = (abs ((alturaArvore arvDir) - (alturaArvore arvEsq)) <= 1) && (estaBalanceada arvDir) && (estaBalanceada arvEsq)
 
-balancear :: ArvoreAlunos a -> ArvoreAlunos a
-balancear
+rotacionarDir :: ArvoreAlunos -> ArvoreAlunos
+rotacionarDir NULL = NULL
+rotacionarDir (No vo (No pai (filhaEsq)(filhaDir)) (tio)) = (No pai (filhaEsq)(No vo (filhaDir)(tio)))
+
+rotacionarEsq :: ArvoreAlunos -> ArvoreAlunos
+rotacionarEsq NULL = NULL
+rotacionarEsq (No vo (tio) (No pai (filhaEsq)(filhaDir))) = (No pai (No vo (tio)(filhaEsq))(filhaDir))
+
+diferencaAltura :: ArvoreAlunos -> Int
+diferencaAltura NULL = 0
+diferencaAltura (No a esq dir) = (alturaArvore esq) - (alturaArvore dir)
+
+balancear :: ArvoreAlunos -> ArvoreAlunos
+balancear NULL = NULL
+balancear (No a esq dir)  
+	|abs (diferencaAltura (No a esq dir)) < 2 = (No a esq dir)
+	|(diferencaAltura (No a esq dir)) == 2 && (diferencaAltura esq) /= -1 = rotacionarDir (No a esq dir)
+	|(diferencaAltura (No a esq dir)) == 2 && (diferencaAltura esq) == -1 = rotacionarDir (No a (rotacionarEsq esq) dir)
+	|(diferencaAltura (No a esq dir)) == -2 && (diferencaAltura esq) /= 1 = rotacionarEsq (No a esq dir)
+	|(diferencaAltura (No a esq dir)) == -2 && (diferencaAltura esq) == -1 = rotacionarEsq (No a esq (rotacionarDir dir))
